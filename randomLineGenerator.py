e16e92363ead92d0b6f6a82509c7b481f427b54e
TheFloatBoat <139195356+TheFloatBoat@users.noreply.github.com>
2025-09-04 17:02:28 +0800

    Create randomLineGenerator.py

---
+++ randomLineGenerator.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+y = np.array([random.randint(0,100),random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100), random.randint(0,100),random.randint(0,100), random.randint(0,100), random.randint(0,100)])
+
+plt.plot(y)
+plt.show()
